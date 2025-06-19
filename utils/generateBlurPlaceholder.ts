55325df577fde21bc68b5659c324b85698cba4e2
Misfit Initiative <104792124+Arktoh@users.noreply.github.com>
2025-06-19 17:34:52 -0400

    configured

--- utils/generateBlurPlaceholder.ts
+++ utils/generateBlurPlaceholder.ts
@@ -15,11 +15,18 @@ export default async function getBase64ImageUrl(
     `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`,
   );
   const buffer = await response.arrayBuffer();
-  const minified = await imagemin.buffer(Buffer.from(buffer), {
-    plugins: [imageminJpegtran()],
-  });
 
-  url = `data:image/jpeg;base64,${Buffer.from(minified).toString("base64")}`;
+  let minified: Buffer;
+  // Bypass imagemin-jpegtran on Windows due to missing jpegtran.exe
+  if (process.platform === 'win32') {
+    minified = Buffer.from(buffer);
+  } else {
+    minified = await imagemin.buffer(Buffer.from(buffer), {
+      plugins: [imageminJpegtran()],
+    });
+  }
+
+  url = `data:image/jpeg;base64,${minified.toString("base64")}`;
   cache.set(image, url);
   return url;
 }
